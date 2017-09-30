73131df5dee4c2dea3c0747a9dfe659dad3ef207
Alessandro <ghezzo_utn@hotmail.com>
2017-09-29 17:47:20 -0700

    dump - Events functions (games and attendants)

--- web/events.php
+++ web/events.php
@@ -1,11 +1,9 @@
 <?php
-    $query_game_retrieve;
-    $query_;
 
     class game
     {
-        private $game_id;
-        private $game_date;
+        private $id;
+        private $date;
         private $attendants = [
                                 "female" => [ 
                                          ],
@@ -16,15 +14,26 @@
                                 "type" => ""
                               ];
 
-        public function __construct ($received_game_type,$received_game_date)
-        {
-            $this->game_id = $received_game_type;
-            $this->game_date = $received_game_date;
+        public function __construct ($received_game_type,$received_game_date){
+            $this->id = $received_game_type;
+            $this->date = $received_game_date;
         }
 
-        public function retrieve()
+        public function retrieve(){
+            echo "Id: ".$this->id." & Date:".$this->date;
+        }
+
+        public function retrieve_attendants(){
+                 return $this->attendants;
+        }
+        public function retrieve_attendant(){
+                 return $this->attendants;
+        }
+
+        public function store()
         {
-            echo "Id: ".$this->game_id." & Date:".$this->game_date;
+            $insert_game = "INSERT VALUES($this->id,$this->date) IN game(id,date)";
+            $insert_game_query = mysqli_query($link,$events_retrieve);
         }
 
     }
@@ -35,7 +44,21 @@
         private $games = [];
         function __construct()
         {
-            $events_retrieve = "";
+            
+
+        }
+        function retrieve()
+        {
+
+            for($i = 0;$i < count($this->games); $i++)
+            {
+                echo $this->games[$i]->retrieve();  
+            }
+        }
+
+        function generate()
+        {
+            $events_retrieve = "SELECT id date FROM game";
             $events_retrieve_query = mysqli_query($link,$events_retrieve);
             $events_retrieve_query_row = mysqli_fetch_assoc($task_query_retrieve);
             $events_retrieve_query_amount = mysqli_num_rows($task_query_retrieve);
@@ -43,22 +66,15 @@
             for ($i=0; $i < $events_retrieve_query_amount; $i++) { 
                 $this->games[i] = new game($events_retrieve_query_row['id'],$events_retrieve_query_row['date']);
             }
+        }
 
-        }
-        function retrieve()
-        {
-            for($i = 0;$i < count($this->games); $i++)
-            {
-                echo $this->games[$i]->retrieve();  
-            }
-        }
+
+
     }
 
-    /*$Games = [];
-    $Games[0]= new game("1","dateHere");
-    $Games[1]= new game("2","dateHere");
-    $Games[2]= new game("3","dateHere");*/
-    
+    $Events = new events;
+    echo $events->retrieve();
+
 
     /**/
 
