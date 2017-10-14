6a3d5db911290ca0e1da9896182fe886319120a2
Alessandro <ghezzo_utn@hotmail.com>
2017-10-13 23:26:07 -0700

    more css cleanup

--- web/login.php
+++ web/login.php
@@ -11,33 +11,35 @@ include("header.html");
     header("location:index.php");
   }
 ?>
-</head>
 
-<body style="background-color:#95A3B3">
+ <link rel="stylesheet" type="text/css" href="css/style.css">
+
+</head>
+<body class="landing">
 
 <div class="container-fluid" style="margin-top:10%;">
 
   <div class="row justify-content-center">
-    <div class="col"></div>
-    <div class="col-4" style="border:1px solid;border-color:#5D2E8C;border-radius:5px;margin:10px;background-color:white">
+    <div class="landing col-4">
     <h5 class="form-text text-muted" style="padding-top:15px">Fúlbo | Welcome</h5>
-    <h6 class="form-text text-muted" style="padding-top:15px">Login</h5>
-    <hr>
+      <h6 class="form-text text-muted" style="padding-top:15px">Login</h5>
+      <hr>
       <form id="login_form" method="POST" action="src/login.php">
      
       <label>Username:</label>
-      <input id="username" style="background:#D8E1FF;border-color:#5D2E8C" type="text" class="form-control" name="username" placeholder="Username">
+      <input type="text" class="form-control landing" name="username" placeholder="Username">
       <label>Password:</label>
-      <input id="password" style="background:#D8E1FF;border-color:#5D2E8C" type="password" class="form-control" name="password" placeholder="Password">
+      <input type="password" class="form-control landing" name="password" placeholder="Password">
       
-    <hr>
-      <button type="submit" style="background-color:white;border-width:1px;border-color: #5D2E8C;border-radius: 3px">Submit</button>  
-      <a href="index.php?id=returned"><input type="button" style="background-color:white;border:1px solid;border-color: #5D2E8C;border-radius: 3px;"  value="Back"/></a>
+      <hr>
+      <button class="content" type="submit">Submit</button>  
+      <a href="index.php?id=returned">
+        <button type="button" class="content">Back</button>
+      </a>
       
       </form>
-    </div> <!-- Col -->
-    <div class="col"></div>
-  </div>
+    </div> <!-- Col4 -->
+  </div> <!-- row -->
 
 </div> <!-- Container -->
 <?php
--- web/register.php
+++ web/register.php
@@ -11,45 +11,50 @@
   }
 
 ?>
+
+ <link rel="stylesheet" type="text/css" href="css/style.css">
+
 </head>
 
-<body style="background-color:#95A3B3">
+<body class="landing">
 
 <div class="container-fluid" style="margin-top:10%;">
 
   <div class="row justify-content-center">
     <div class="col"></div>
-    <div class="col-4" style="border:1px solid;border-color:#5D2E8C;border-radius:5px;margin:10px;background-color:white">
+    <div class="col-4 landing">
     <h5 class="form-text text-muted" style="padding-top:15px">Fúlbo | Welcome</h5>
     <h6 class="form-text text-muted" style="padding-top:15px">Register</h5>
     <hr>
       <form id="login_form" method="POST" action="src/register.php">
      
       <label>Username:</label>
-      <input style="background:#D8E1FF;border-color:#5D2E8C;width:100%" type="text" class="form-control" name="username" placeholder="Username">
+      <input class="form-control landing" name="username" placeholder="Username">
       <small class="form-text text-muted">Username must be unique.</small>
       <label>Password:</label>
-      <input style="background:#D8E1FF;border-color:#5D2E8C;width:100%" type="password" class="form-control" name="password" placeholder="Password">
+      <input class="form-control landing" name="password" placeholder="Password">
       <small class="form-text text-muted">Use a random password.</small>
       <label>Nickname:</label>
-      <input style="background:#D8E1FF;border-color:#5D2E8C;width:100%" type="text" class="form-control" name="nickname" placeholder="Shorty">
+      <input class="form-control landing" name="nickname" placeholder="Shorty">
       <small class="form-text text-muted">Whatever you wanna be called.</small><br>
       <label>Gender:</label>
-      <select style="background:#D8E1FF;border-color:#5D2E8C;border-radius:5%" name="gender">
-          <option style="background:#D8E1FF;border-color:#5D2E8C;border-radius:5%">Male</option>
-          <option style="background:#D8E1FF;border-color:#5D2E8C;border-radius:5%">Female</option>
+      <select class="landing" name="gender">
+          <option class="landing">Male</option>
+          <option class="landing">Female</option>
       </select>
       <label>Skill:</label>
-      <select style="background:#D8E1FF;border-color:#5D2E8C;border-radius:5%" name="skill">
-          <option style="background:#D8E1FF;border-color:#5D2E8C;border-radius:5%">1</option>
-          <option style="background:#D8E1FF;border-color:#5D2E8C;border-radius:5%">2</option>
-          <option style="background:#D8E1FF;border-color:#5D2E8C;border-radius:5%">3</option>
-          <option style="background:#D8E1FF;border-color:#5D2E8C;border-radius:5%">4</option>
-          <option style="background:#D8E1FF;border-color:#5D2E8C;border-radius:5%">5</option>
+      <select class="landing" name="skill">
+          <option class="landing">1</option>
+          <option class="landing">2</option>
+          <option class="landing">3</option>
+          <option class="landing">4</option>
+          <option class="landing">5</option>
       </select>
     <hr>
-      <button type="submit" style="background-color:white;border:1px solid;border-color: #5D2E8C;border-radius: 3px;">Submit</button>  
-      <a href="index.php?id=returned"><input type="button" style="background-color:white;border:1px solid;border-color: #5D2E8C;border-radius: 3px;"  value="Back"/></a>
+      <button class="content" type="submit">Submit</button>  
+      <a href="index.php?id=returned">
+        <button type="button" class="content">Back</button>
+      </a>
       </form>
     </div> <!-- Col -->
     <div class="col"></div>
