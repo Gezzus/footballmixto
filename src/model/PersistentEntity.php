2410bb5678d73e1704cfe89bf544e0d27501740d
Claudio Barril <claudio.barril@avature.net>
2018-01-13 17:57:56 -0300

    working in Player model class

--- src/model/PersistentEntity.php
+++ src/model/PersistentEntity.php
@@ -4,22 +4,22 @@ include_once "../config/Database.php";
 
 class PersistentEntity {
 
-    public function sanitize($value) {
+    public static function sanitize($value) {
         return Database::getInstance()->getConn()->quote($value);
     }
 
-    public function lastInsertId() {
+    public static function lastInsertId() {
         return Database::getInstance()->getConn()->lastInsertId();
     }
 
-    public function query($query) {
+    public static function query($query) {
         return Database::getInstance()->getConn()->query($query);
     }
 
-    public function queryWithParameters($query, $params) {
+    public static function queryWithParameters($query, $params) {
         $statement = Database::getInstance()->getConn()->prepare($query);
         for ($i = 0; $i < count($params); $i++) {
-            $statement->bindParam($i + 1, $this->sanitize($params[$i]));
+            $statement->bindParam($i + 1, self::sanitize($params[$i]));
         }
         $statement->execute();
         return $statement;
--- src/model/Player.php
+++ src/model/Player.php
@@ -9,26 +9,32 @@
 		private $genderId;
 		private $levelId;
 
-		
-
-		function __construct($id, $nickName, $genderId, $levelId) {
+		public function __construct($id, $nickName, $genderId, $levelId) {
             $this->id = $id;
             $this->nickName = $nickName;
             $this->genderId = $genderId;
             $this->levelId = $levelId;
-		}
+        }
 
-		private function create($nickName, $genderId) {
-			$player = $this->queryWithParameters("SELECT * FROM player WHERE nickName = ? AND genderId = ?", array($nickName, $genderId));
-            if(count($player->fetchAll()) == 0){
-                $this->queryWithParameters("INSERT INTO player(nickName, genderId) VALUES(?, ?)", array($nickName, $genderId));
-				$this->id = $this->lastInsertId();
-				return $this->toJson();
-			}
-			else{
+        private static function create($nickName, $genderId) {
+			$dbPlayer = self::queryWithParameters("SELECT * FROM player WHERE nickName = ? AND genderId = ?", array($nickName, $genderId));
+            if($dbPlayer->rowCount() == 0) {
+                self::queryWithParameters("INSERT INTO player(nickName, genderId) VALUES(?, ?)", array($nickName, $genderId));
+				return self::get(self::lastInsertId());
+			} else{
 				return null;
 			}
 		}
+
+        private static function get($playerId){
+            $dbPlayer = self::queryWithParameters("SELECT * FROM player WHERE id = ?", array($playerId));
+            if($dbPlayer->rowCount() == 1) {
+                $playerData = $dbPlayer->fetch();
+                return new Player($playerData["id"], $playerData["nickName"], $playerData["genderId"], $playerData["levelId"]);
+            } else {
+                return null;
+            }
+        }
 
         public function toJson() {
             $return = [
@@ -40,55 +46,6 @@
             return json_encode($return);
         }
 
-        private function get($playerId){
-            $player = $this->queryWithParameters("SELECT * FROM player WHERE id = ?", array($playerId));
-            if(count($player->fetchAll()) == 1){
-
-            }
-
-            $query_find_player = "SELECT * FROM player WHERE id = '" . $received_id . "' LIMIT 1";
-			$result_query_find_player = $mysqli->query($query_find_player);
-			$result_amount = $result_query_find_player->num_rows;
-			if($result_amount == 1){
-				return $response = [200,$result_query_find_player->fetch_array];
-			}
-			else{
-				$response = [201,"Id doesn't exist."];
-			}
-		}
-
-        public function __construct_existing($received_player_id,$mysqli){
-			$this->properties["id"] = $received_player_id;
-			$response = $this->db_retrieve_player($received_player_id,$mysqli);
-			print_r($response);
-		}
-        public function __construct_new($received_player,$mysqli){
-			$response = $this->db_create_player($received_player[0],$received_player[1],$mysqli);
-
-			if($response[0] == 200){
-				$this->properties["id"] = $response[0];
-				$this->properties["nickname"] = $received_player[0];
-				$this->properties["gender"] = $received_player[1];
-				$response[0] = [200,null];
-				print_r($this->properties);
-				return $response;
-			}
-			else{
-				return $response;
-			}
-
-		}
-
-        #function __construct($received_id);
-
-        public function retrieve(){
-			return $this;
-		}
     }
-
-
-
-	
-
 ?>
 
