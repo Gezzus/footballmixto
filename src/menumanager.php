7f5586a84d6390ce65f6fd686c554fff70297e10
Claudio Barril <claudio.barril@avature.net>
2018-01-27 17:03:23 -0300

    identation

--- src/menumanager.php
+++ src/menumanager.php
@@ -1,31 +1,25 @@
 <?php
 
-function stop_session($activelink)
-{
+function stop_session($activelink) {
 	session_destroy($activelink);
 }
 
 
-function active_session()
-{
-	if(isset($_SESSION['id']))
-	{
-	return(1);
+function active_session() {
+	if(isset($_SESSION['id']))  {
+		return(1);
+	} else {
+		return(0);
 	}
-	else
-	{return(0);}
 }
 
-function isAdmin($thisId,$activelink)
-{
-	$task_retrieve = "SELECT roleId FROM user WHERE id='".$thisId."'";
-	$task_query_retrieve = mysqli_query($activelink,$task_retrieve);
+function isAdmin($thisId, $activelink) {
+	$task_retrieve = "SELECT roleId FROM user WHERE id='" . $thisId . "'";
+	$task_query_retrieve = mysqli_query($activelink, $task_retrieve);
 	$task_query_row = mysqli_fetch_assoc($task_query_retrieve);
-	if($task_query_row["roleId"] == 2){
+	if($task_query_row["roleId"] == 2) {
 		return 1;
-	}
-	else{
+	} else{
 		return 0;
 	}
 }
-?>
