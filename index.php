add7a9a7789516fa99b02d67a1a158c6141cfa7b
Pablo Angelani <pablo.angelani@gmail.com>
2019-11-14 22:45:50 -0300

    Add views

--- index.php
+++ index.php
@@ -30,6 +30,10 @@ $container['errorHandler'] = function ($c) {
     };
 };
 
+$container['view'] = function ($container) {
+    return new \Slim\Views\PhpRenderer('web/');
+};
+
 $app->add(function (Request $request, Response $response, $next) {
   $path = $request->getUri()->getPath();
   if (substr($path, 0, 4) === 'api/' && $path !== 'api/login' && !Session::getInstance()->hasLoggedInUser()) {
@@ -42,7 +46,49 @@ $app->add(function (Request $request, Response $response, $next) {
 
 // Single Page App
 $app->get('/', function (Request $request, Response $response) {
-  return $response->withRedirect(Session::getInstance()->hasLoggedInUser() ? '/web/index.html' : '/web/login.html');
+  return $response->withRedirect(Session::getInstance()->hasLoggedInUser() ? '/web/home' : '/web/login');
+});
+
+$app->get('/web/login', function (Request $request, Response $response) {
+  return Session::getInstance()->hasLoggedInUser()
+    ? $response->withRedirect('/web/home')
+    : $this->view->render($response, 'login.html', []);
+});
+
+$app->get('/web/register', function (Request $request, Response $response) {
+  return Session::getInstance()->hasLoggedInUser()
+    ? $response->withRedirect('/web/home')
+    : $this->view->render($response, 'register.html', []);
+});
+
+$app->get('/web/home', function (Request $request, Response $response) {
+  return Session::getInstance()->hasLoggedInUser()
+    ? $this->view->render($response, 'index.html', [])
+    : $response->withRedirect('/web/login');
+});
+
+$app->get('/web/event', function (Request $request, Response $response) {
+  return Session::getInstance()->hasLoggedInUser()
+    ? $this->view->render($response, 'game.html', [])
+    : $response->withRedirect('/web/login');
+});
+
+$app->get('/web/events', function (Request $request, Response $response) {
+  return Session::getInstance()->hasLoggedInUser()
+    ? $this->view->render($response, 'events.html', [])
+    : $response->withRedirect('/web/login');
+});
+
+$app->get('/web/admin', function (Request $request, Response $response) {
+  return Session::getInstance()->hasLoggedInUser()
+    ? $this->view->render($response, 'admin.html', [])
+    : $response->withRedirect('/web/login');
+});
+
+$app->get('/web/profile', function (Request $request, Response $response) {
+  return Session::getInstance()->hasLoggedInUser()
+    ? $response->withRedirect(Session::getInstance()->hasLoggedInUser() ? '/web/index.html' : '/web/login.html')
+    : $response->withRedirect('/web/login');
 });
 
 $app->get('/api/logout', function (Request $request, Response $response) {
